4cc440144faa91c601e6500129bdfede91ee44c1
Saul Augusto Gasca Farrera <sgfarrera115@hotmail.com>
2021-04-20 12:15:26 -0500

    Home and Register Components,Hook Up the register method to the service and add comunicaction between child to parent component and vice versa

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -9,25 +11,40 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
 
   model: any = {}
-  loggedIn: boolean
+  //loggedIn: boolean
+  //currentUser$: Observable<User>
 
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
+
+    //this.getCurrentUser();
+    //this.currentUser$ = this.accountService.currentUser$;
   }
 
   login(){
     //console.log(this.model)
     this.accountService.login(this.model).subscribe(response => {
       console.log(response)
-      this.loggedIn = true;
+      //this.loggedIn = true;
     }, error =>{
       console.log(error)
     })
   }
 
   logout(){
-    this.loggedIn = false;
+    this.accountService.logout();
+    //this.loggedIn = false;
   }
+
+  /*getCurrentUser()
+  {
+    this.accountService.currentUser$.subscribe(user =>{
+      this.loggedIn = !!user
+    },err => {
+      console.log(err)
+    })
+    
+  }*/
 
 }
